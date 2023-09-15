c1f0b6dc12557f1d7fa5e948f18ca5a3a43281d2
wesleybrien <47414300+wesleybrien@users.noreply.github.com>
2023-09-15 16:51:46 +0100

    Update app2.js

--- src/app2.js
+++ src/app2.js
@@ -1,8 +1,11 @@
 // Import the library
 var asana = require('asana');
 
+//const API_KEY = '1/1205507933696308:2995f4a3f942b610bf8431a8ba26768b';
+//const WORKSPACE_ID = '1205508065440795';
+//const PROJECT_ID = '1205508197426998';
 // Note: Replace this value with your own personal access token
-var personalAccessToken = '0/123456789....';
+var personalAccessToken = '1/1205507933696308:2995f4a3f942b610bf8431a8ba26768b';
 
 // Construct an Asana client
 var client = asana.Client.create().useAccessToken(personalAccessToken);
