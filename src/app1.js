6709d210384713717e066a23b11ca32daa3fa8f3
wesleybrien <47414300+wesleybrien@users.noreply.github.com>
2023-09-15 15:03:01 +0100

    Update app1.js

--- src/app1.js
+++ src/app1.js
@@ -52,25 +52,6 @@ function App() {
       pdf.save('order.pdf');
     });
   }
-
-  function asana() {
-    // Import the library
-var asana = require('asana');
-
-// Note: Replace this value with your own personal access token
-var personalAccessToken = '0/1205508112784251';
-
-// Construct an Asana client
-var client = asana.Client.create().useAccessToken(personalAccessToken);
-
-// Get your user info
-client.users.getUser("me")
-  .then(function(me) {
-    // Print out your information
-    console.log('Hello world! ' + 'My name is ' + me.name + '!');
-});
-  );
-  }
   
   return (
     <div>
