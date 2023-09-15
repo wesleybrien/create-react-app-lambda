f609c65fa431612c8b4b8de6fc693868c00c2f32
wesleybrien <47414300+wesleybrien@users.noreply.github.com>
2023-09-15 16:48:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import App from './app1';
+import App from './app2';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
