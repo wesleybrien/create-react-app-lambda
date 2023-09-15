147f5b2f4829e48a6d1f3f2e709b18e283081e2b
wesleybrien <47414300+wesleybrien@users.noreply.github.com>
2023-09-15 14:03:03 +0100

    Update app1.js

--- src/app1.js
+++ src/app1.js
@@ -77,7 +77,23 @@ function App() {
         <p>Toppings: {toppings.join(", ")}</p>
       </div>
       <button onClick={handleDownload}>Download PDF</button>
-    wes</div>
+    </div>
+
+  // Import the library
+var asana = require('asana');
+
+// Note: Replace this value with your own personal access token
+var personalAccessToken = '0/123456789....';
+
+// Construct an Asana client
+var client = asana.Client.create().useAccessToken(personalAccessToken);
+
+// Get your user info
+client.users.getUser("me")
+  .then(function(me) {
+    // Print out your information
+    console.log('Hello world! ' + 'My name is ' + me.name + '!');
+});
   );
 }
 
